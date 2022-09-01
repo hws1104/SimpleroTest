17ca9a82a13abcbe13723cc7417c1cb6d09912a9
Gabriel Freitas <102642303+hws1104@users.noreply.github.com>
2022-09-01 11:17:44 -0300

    Update Header with fontawesome icons

--- app/javascript/src/plugins/fontawesome.js
+++ app/javascript/src/plugins/fontawesome.js
@@ -8,12 +8,12 @@ config.autoAddCss = false;
 // An example icon
 import {faFacebookSquare, faTwitter, faGoogle, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
-import {faThList, faSignInAlt, faCog, faSignOutAlt, faUserGraduate, faStaffAesculapius, faBuildingColumns} from '@fortawesome/free-solid-svg-icons';
+import {faThList, faSignInAlt, faCog, faSignOutAlt, faUserGraduate, faStaffAesculapius, faBuildingColumns, faBars} from '@fortawesome/free-solid-svg-icons';
 
 import {faAngry} from '@fortawesome/free-regular-svg-icons';
 
 library.add(
-    faThList, faSignInAlt, faFacebookSquare, faTwitter, faGoogle, faLinkedin, faCog, faSignOutAlt,faUserGraduate,faStaffAesculapius,faBuildingColumns
+    faThList, faSignInAlt, faFacebookSquare, faTwitter, faGoogle, faLinkedin, faCog, faSignOutAlt,faUserGraduate,faStaffAesculapius,faBuildingColumns, faBars
 );
 
 
